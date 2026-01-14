f731d4775317d7071c966650f48ed5d428ae2660
ChetnaMahajan02 <chetna.chetna@gmail.com>
2026-01-14 13:37:56 +0530

    ADD file_handler.py

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,1 +1,19 @@
 ## Task 1.1  Read Sales data with Encoding handling
+
+import csv
+
+def read_sales_data(filename, file_encoder):
+    try:
+        data= []
+        with open(filename, mode= "r", encoding= file_encoder, newline= "\n") as file:
+            file_data= csv.reader(file, delimiter="|")
+            next(file_data)   # skips header
+            for row in file_data:
+                if row and any(field.strip() for field in row):  # removes empty lines
+                    print (row)
+        return data
+    except FileNotFoundError:
+        print ("File not found")
+    return []
+
+read_sales_data("data/sales_data.txt", "utf-8")
